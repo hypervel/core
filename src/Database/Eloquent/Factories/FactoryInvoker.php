6691e593177963024d3fbb6556632b97dd382510
Pratik PAWAR <p.pawar@planetsmartcity.com>
2025-05-24 12:37:25 +0530

    - Enabled configuration of the Faker locale. - Implemented tests to verify proper Faker locale usage. - Refactored the factory method to leverage the `ModelFactory` helper.

--- src/Database/Eloquent/Factories/FactoryInvoker.php
+++ src/Database/Eloquent/Factories/FactoryInvoker.php
@@ -5,14 +5,17 @@ declare(strict_types=1);
 namespace Hypervel\Database\Eloquent\Factories;
 
 use Faker\Factory as FakerFactory;
+use Hyperf\Contract\ConfigInterface;
 use Psr\Container\ContainerInterface;
 
 class FactoryInvoker
 {
     public function __invoke(ContainerInterface $container)
     {
+        $config = $container->get(ConfigInterface::class);
+
         $factory = new Factory(
-            FakerFactory::create('en_US')
+            FakerFactory::create($config->get('app.faker_locale', 'en_US'))
         );
 
         if (is_dir($path = database_path('factories') ?: '')) {
