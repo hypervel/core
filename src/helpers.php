d38d9d085ae95ce4f553f38a7b1faf3dad110974
Richard Anabeto Opoku <anabeto93@gmail.com>
2025-07-26 17:28:06 +0000

    return a proper context instance and not just a class string.

--- src/helpers.php
+++ src/helpers.php
@@ -8,33 +8,34 @@ if (! function_exists('context')) {
      *
      * @param  array|string|null  $key
      * @param  mixed  $default
-     * @param int|null $coroutineId
+     * @param  int|null  $coroutineId
      * @return mixed
      */
-    function context($key = null, $default = null, ?int $coroutineId = null)
+    function context(array|string|null $key = null, mixed $default = null, ?int $coroutineId = null): mixed
     {
+        /**
+         * 
+         * Just want to make sure that the context is an object that has all methods of \Hypervel\Context\Context class.
+         * Could have used \Hyperf\Engine\Coroutine::getContextFor($coroutineId) instead but prefer to rely on hypervel's Context class.
+         * This way, we can ensure that the context is always an instance of Hypervel\Context\Context
+         *  @see \Hypervel\Context\Context::get()
+         * 
+         * @var \Hypervel\Context\Context|array $context
+         */
+        $context = new class extends \ArrayObject {
+            public function __call(string $method, array $arguments): mixed
+            {
+                return \Hypervel\Context\Context::{$method}(...$arguments);
+            }
+        };
         return match (true) {
-            is_null($key) => \Hypervel\Context\Context::class,
-            is_array($key) => context_set_multiple($key, $coroutineId),
+            is_null($key) => $context,
+            is_array($key) => tap($context, function () use ($key, $coroutineId) {
+                foreach ($key as $k => $v) {
+                    \Hypervel\Context\Context::set($k, $v, $coroutineId);
+                }
+            }),
             default => \Hypervel\Context\Context::get($key, $default, $coroutineId),
         };
-    }
-}
-
-if (! function_exists('context_set_multiple')) {
-    /**
-     * Set multiple context values at once.
-     *
-     * @param  array  $values
-     * @param int|null $coroutineId
-     * @return string
-     */
-    function context_set_multiple(array $values, ?int $coroutineId = null): string
-    {
-        foreach ($values as $key => $value) {
-            \Hypervel\Context\Context::set($key, $value, $coroutineId);
-        }
-        
-        return \Hypervel\Context\Context::class;
     }
 }
